4e817887ade27604ef045a6d3caf571d768fb1b2
merefield <merefield@gmail.com>
2025-04-25 23:18:41 +0100

    update mime deps

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-discord-bot
 # about: Integrate Discord Bots with Discourse
-# version: 0.3.25
+# version: 0.3.26
 # authors: Robert Barrow
 # url: https://github.com/merefield/discourse-discord-bot
 
@@ -15,8 +15,8 @@ gem 'websocket', '1.2.11'
 gem 'websocket-client-simple', '0.3.0'
 gem 'opus-ruby', '1.0.1', { require: false }
 gem 'netrc', '0.11.0'
-gem 'mime-types-data', '3.2019.1009'
-gem 'mime-types', '3.3.1'
+gem 'mime-types-data', '3.2025.0422', { require: false }
+gem 'mime-types', '3.6.2', { require: false }
 gem 'domain_name', '0.5.20180417'
 gem 'http-cookie', '1.0.3'
 gem 'http-accept', '1.7.0', { require: false }
