ed2b62e64b058beaf301527f7ce6b40501abfa7a
merefield <merefield@gmail.com>
2026-09-01 10:30:34 +0100

    FIX: Initialize Discord API logging

--- lib/discordrb_loader.rb
+++ lib/discordrb_loader.rb
@@ -2,18 +2,43 @@
 
 module ::DiscordBot
   module DiscordrbLoader
+    LOAD_MUTEX = Mutex.new
+
     module_function
 
     def load
-      require "discordrb" unless defined?(::Discordrb::Commands::CommandBot)
+      return if defined?(::Discordrb::Commands::CommandBot)
+
+      LOAD_MUTEX.synchronize do
+        return if defined?(::Discordrb::Commands::CommandBot)
+
+        if @api_logger_initialized && ::Discordrb.const_defined?(:LOGGER, false)
+          ::Discordrb.send(:remove_const, :LOGGER)
+          @api_logger_initialized = false
+        end
+
+        require "discordrb"
+      end
     end
 
     def load_api
-      return if defined?(::Discordrb::API::Channel)
+      return if defined?(::Discordrb::API::Channel) && defined?(::Discordrb::LOGGER)
 
-      require "discordrb/version"
-      require "discordrb/api"
-      require "discordrb/api/channel"
+      LOAD_MUTEX.synchronize do
+        return if defined?(::Discordrb::API::Channel) && defined?(::Discordrb::LOGGER)
+
+        require "discordrb/version"
+        require "discordrb/logger"
+        unless ::Discordrb.const_defined?(:LOGGER, false)
+          ::Discordrb.const_set(
+            :LOGGER,
+            ::Discordrb::Logger.new(ENV.fetch("DISCORDRB_FANCY_LOG", false)),
+          )
+          @api_logger_initialized = true
+        end
+        require "discordrb/api"
+        require "discordrb/api/channel"
+      end
     end
   end
 end
